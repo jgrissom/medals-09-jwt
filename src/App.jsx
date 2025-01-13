7c1bebcdd272a535c3b4e1cbe05902ad76adfa30
jgrissom <jgrissom91@gmail.com>
2025-01-13 17:29:22 -0600

    [5.06] Additonal countries

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,16 @@ function App() {
     { id: 1, name: "United States", gold: 2, silver: 2, bronze: 3 },
     { id: 2, name: "China", gold: 3, silver: 1, bronze: 0 },
     { id: 3, name: "France", gold: 0, silver: 2, bronze: 2 },
+    { id: 4, name: "Germany", gold: 0, silver: 2, bronze: 2 },
+    { id: 5, name: "Spain", gold: 1, silver: 1, bronze: 0 },
+    { id: 6, name: "United Kingdom", gold: 0, silver: 2, bronze: 3 },
+    { id: 7, name: "Brazil", gold: 3, silver: 0, bronze: 0 },
+    { id: 8, name: "Italy", gold: 2, silver: 2, bronze: 2 },
+    { id: 9, name: "Switzerland", gold: 1, silver: 1, bronze: 2 },
+    { id: 10, name: "Poland", gold: 0, silver: 2, bronze: 1 },
+    { id: 11, name: "Sweden", gold: 0, silver: 3, bronze: 1 },
+    { id: 12, name: "Ireland", gold: 2, silver: 1, bronze: 0 },
+    { id: 13, name: "Scotland", gold: 3, silver: 0, bronze: 2 },
   ]);
   const medals = useRef([
     { id: 1, name: "gold" },
